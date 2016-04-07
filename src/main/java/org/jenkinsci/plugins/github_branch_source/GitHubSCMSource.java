a78dd3a4e74889a7ac662fb689cc9d80d7f3bdc8
Manuel Recena <recena@gmail.com>
2016-04-07 06:55:10 +0200

    Removed an unused import

--- src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMSource.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMSource.java
@@ -77,7 +77,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.Comparator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
