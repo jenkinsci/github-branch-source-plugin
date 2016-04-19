6d0283ffae0e290a29ee583eeb758f03986abc0f
recena <mrecena@cloudbees.com>
2016-04-19 10:50:38 +0200

    Fix typo

--- src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMSource.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMSource.java
@@ -447,7 +447,7 @@ public class GitHubSCMSource extends AbstractGitSCMSource {
         @Restricted(NoExternalUse.class)
         public FormValidation doCheckIncludes(@QueryParameter String value) {
             if (value.isEmpty()) {
-                return FormValidation.warning(Messages.GitHubSCMSource_did_you_mean_to_use_to_match_all_branche());
+                return FormValidation.warning(Messages.GitHubSCMSource_did_you_mean_to_use_to_match_all_branches());
             }
             return FormValidation.ok();
         }
