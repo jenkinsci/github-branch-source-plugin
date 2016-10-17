7872bf2eea07ee7a6c9e54c32bc5d63f8eabdbbe
Stephen Connolly <stephen.alan.connolly@gmail.com>
2016-10-17 11:37:36 +0100

    [JENKINS-39026 Follow-up] Add ViewBranchFilter implementations for PRs and GitHub branches

---
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubBranchFilter.java
@@ -0,0 +1,70 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.jenkinsci.plugins.github_branch_source;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.views.ViewJobFilter;
+import jenkins.branch.Branch;
+import jenkins.branch.SimpleViewBranchFilter;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * A {@link ViewJobFilter} that matches {@link PullRequestSCMHead} based branches.
+ *
+ * @since FIXME
+ */
+public class GitHubBranchFilter extends SimpleViewBranchFilter {
+    /**
+     * Our constructor.
+     */
+    @DataBoundConstructor
+    public GitHubBranchFilter() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isIncluded(Branch branch) {
+        return branch.getHead() instanceof BranchSCMHead;
+    }
+
+    /**
+     * Our descriptor.
+     */
+    @Extension(optional = true)
+    public static class DescriptorImpl extends Descriptor<ViewJobFilter> {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getDisplayName() {
+            return Messages.GitHubBranchFilter_DisplayName();
+        }
+    }
+
+}
