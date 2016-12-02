jenkinsci/github-branch-source-plugin

commit d0b46e9d2ba3c22e821f52d82b6c6a080ca4e4bb

apiUrl api.github.com requires special case repository resolving
Closes #83

src/main/java/org/jenkinsci/plugins/github_branch_source/RepositoryUriResolver.java
@@ -38,7 +38,9 @@ public abstract class RepositoryUriResolver {
         if (apiUri != null) {
             try {
                 URL endpoint = new URL(apiUri);
-                return endpoint.getHost();
+                if (!"api.github.com".equals(endpoint.getHost()))){
+                    return endpoint.getHost();
+                }
             } catch (MalformedURLException e) {
                 // ignore
             }

commit 72d295b1631dc14c305402273fd9d95032a11b83

Fixing syntax error introduced in d0b46e9d2ba3c22e821f52d82b6c6a080ca4e4bb when merging #83.

src/main/java/org/jenkinsci/plugins/github_branch_source/RepositoryUriResolver.java
@@ -38,7 +38,7 @@ public abstract class RepositoryUriResolver {
         if (apiUri != null) {
             try {
                 URL endpoint = new URL(apiUri);
-                if (!"api.github.com".equals(endpoint.getHost()))){
+                if (!"api.github.com".equals(endpoint.getHost())) {
                     return endpoint.getHost();
                 }
             } catch (MalformedURLException e) {