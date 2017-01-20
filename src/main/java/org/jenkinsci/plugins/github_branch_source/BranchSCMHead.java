fd0d732116d2b940e4a8bc543e05e0c1c76b886e
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-01-20 17:47:35 +0000

    [JENKINS-41121] Implement the SCMHeadMigration API

--- src/main/java/org/jenkinsci/plugins/github_branch_source/BranchSCMHead.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/BranchSCMHead.java
@@ -25,8 +25,13 @@
 package org.jenkinsci.plugins.github_branch_source;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import jenkins.plugins.git.AbstractGitSCMSource;
 import jenkins.scm.api.SCMHead;
-import jenkins.scm.api.SCMHeadCategory;
+import jenkins.scm.api.SCMHeadMigration;
+import jenkins.scm.api.SCMRevision;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 /**
  * Head corresponding to a branch.
@@ -46,5 +51,24 @@ public class BranchSCMHead extends SCMHead {
     @Override
     public String getPronoun() {
         return Messages.BranchSCMHead_Pronoun();
+    }
+
+    @Restricted(NoExternalUse.class)
+    @Extension
+    public static class MigrationImpl extends SCMHeadMigration<GitHubSCMSource, SCMHead, AbstractGitSCMSource.SCMRevisionImpl> {
+        public MigrationImpl() {
+            super(GitHubSCMSource.class, SCMHead.class, AbstractGitSCMSource.SCMRevisionImpl.class);
+        }
+
+        @Override
+        public SCMHead migrate(@NonNull GitHubSCMSource source, @NonNull SCMHead head) {
+            return new BranchSCMHead(head.getName());
+        }
+
+        @Override
+        public SCMRevision migrate(@NonNull GitHubSCMSource source,
+                                   @NonNull AbstractGitSCMSource.SCMRevisionImpl revision) {
+            return new AbstractGitSCMSource.SCMRevisionImpl(migrate(source, revision.getHead()), revision.getHash());
+        }
     }
 }
