4aa561232a24e9d91d09cefbc6958047b24f21ac
Zhao Xiaojie <rick@jenkins-zh.cn>
2019-11-05 08:29:11 +0800

    Update src/main/java/org/jenkinsci/plugins/github_branch_source/OriginPullRequestDiscoveryTrait.java
    Co-Authored-By: Liam Newman <bitwiseman@gmail.com>

--- src/main/java/org/jenkinsci/plugins/github_branch_source/OriginPullRequestDiscoveryTrait.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/OriginPullRequestDiscoveryTrait.java
@@ -136,7 +136,7 @@ public class OriginPullRequestDiscoveryTrait extends SCMSourceTrait {
          */
         @Override
         public String getDisplayName() {
-            return Messages.discover_pull_requests_from_origin();
+            return Messages.OriginPullRequestDiscoveryTrait_discoverPullRequestsFromOrigin();
         }
 
         /**
