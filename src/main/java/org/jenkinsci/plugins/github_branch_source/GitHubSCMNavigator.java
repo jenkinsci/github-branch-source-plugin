60996772228f22933c8aa7cca2fb6055565f3929
Paul Salaberria <psalaberria002@gmail.com>
2020-05-15 11:13:13 +0200

    Update GitHubSCMNavigator.java

--- src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.java
@@ -933,7 +933,10 @@ public class GitHubSCMNavigator extends SCMNavigator {
                 WitnessImpl witness = new WitnessImpl(listener);
 
                 boolean githubAppAuthentication = credentials instanceof GitHubAppCredentials;
-                if (!github.isAnonymous() && !githubAppAuthentication) {
+                if (github.isAnonymous()) {
+                    listener.getLogger().format("Connecting to %s with no credentials, anonymous access%n",
+                            apiUri == null ? GitHubSCMSource.GITHUB_URL : apiUri);                
+                } else if (!githubAppAuthentication) {
                     GHMyself myself;
                     try {
                         // Requires an authenticated access
