72db8334dbb9203215a941c94ea1ef0b12cc5e84
Jose Blas Camacho Taboada <joseblas@gmail.com>
2020-10-21 08:54:36 +0200

    JENKINS-59016 fixing issues with certificates when using org (#354)
    * JENKINS-59016 fixing issues with certificates when using org

--- src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.java
@@ -99,6 +99,7 @@ import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.jenkinsci.plugins.github_branch_source.Connector.isCredentialValid;
 
 public class GitHubSCMNavigator extends SCMNavigator {
@@ -261,8 +262,11 @@ public class GitHubSCMNavigator extends SCMNavigator {
      */
     @DataBoundSetter
     public void setApiUri(String apiUri) {
-        apiUri = GitHubConfiguration.normalizeApiUri(Util.fixEmptyAndTrim(apiUri));
-        this.apiUri = GitHubServerConfig.GITHUB_URL.equals(apiUri) ? null : apiUri;
+        if(isBlank(apiUri)){
+            this.apiUri = GitHubServerConfig.GITHUB_URL;
+        }else {
+            this.apiUri = GitHubConfiguration.normalizeApiUri(Util.fixEmptyAndTrim(apiUri));
+        }
     }
 
     /**
@@ -1436,6 +1440,7 @@ public class GitHubSCMNavigator extends SCMNavigator {
         public SCMNavigator newInstance(String name) {
             GitHubSCMNavigator navigator = new GitHubSCMNavigator(name);
             navigator.setTraits(getTraitsDefaults());
+            navigator.setApiUri(GitHubServerConfig.GITHUB_URL);
             return navigator;
         }
 
