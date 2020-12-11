84cc9314ffb915db396b7cbec1feb8f97fef06bd
Tim Jacomb <timjacomb1+github@gmail.com>
2020-12-11 15:28:31 +0000

    Simplify code

--- src/main/webapp/github-scm-source.js
+++ src/main/webapp/github-scm-source.js
@@ -5,24 +5,22 @@ Behaviour.specify("input[name$=_configuredByUrlRadio]", 'GitHubSCMSourceRadioCon
         return;
     }
 
-    var configuredByUrlInput = e.closest('div[name="source"]').querySelector('input[name="configuredByUrl"]');
+    var configuredByUrlInput = e.closest('[name="source"]').querySelector('input[name="configuredByUrl"]');
     if (configuredByUrlInput) {
         e.onclick = function() {
             configuredByUrlInput.value = e.value;
             // When changing to true the event is triggered.
             if (e.value === "false") {
                 // When changing to false a trigger has to be fired in order to fetch the repos from the backend
-                if (document.createEvent) {
-                    var oEvent = document.createEvent("HTMLEvents");
-                    oEvent.initEvent("change");
-                    // Gets the first Jelly entry after the hidden value
-                    var repoOwner = e.closest('.radioBlock-container').querySelector('input[name="_.repoOwner"]');
-                    if (repoOwner != null) {
-                        // fire a onchange event on the repoOwner input test to get the repos from backend
-                        repoOwner.dispatchEvent(oEvent);
-                    } else {
-                        console.warn("couldn't find repoOwner element", e)
-                    }
+                var oEvent = document.createEvent("HTMLEvents");
+                oEvent.initEvent("change");
+                // Gets the first Jelly entry after the hidden value
+                var repoOwner = e.closest('.radioBlock-container').querySelector('input[name="_.repoOwner"]');
+                if (repoOwner != null) {
+                    // fire a onchange event on the repoOwner input test to get the repos from backend
+                    repoOwner.dispatchEvent(oEvent);
+                } else {
+                    console.warn("couldn't find repoOwner element", e)
                 }
             }
         };
