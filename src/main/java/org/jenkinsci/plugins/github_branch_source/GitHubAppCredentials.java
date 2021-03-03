5ce0e3719ebd802ace10a9f92f6cb882efac6dba
Carlos Sanchez <carlos@apache.org>
2021-03-03 18:37:51 +0100

    fix: JENKINS-65006 better error message
    when app is installed in two organizations
    and owner is not set in the credential

--- src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubAppCredentials.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubAppCredentials.java
@@ -52,6 +52,8 @@ public class GitHubAppCredentials extends BaseStandardCredentials implements Sta
     private static final String NOT_INSTALLED = ", has it been installed to your GitHub organisation / user?";
 
     private static final String ERROR_NOT_INSTALLED = ERROR_AUTHENTICATING_GITHUB_APP + NOT_INSTALLED;
+    private static final String ERROR_NO_OWNER_MATCHING = "Found multiple installations for GitHub app ID %s but none match credential owner \"%s\". "
+        + "Set the right owner in the credential advanced options";
 
     /**
      * When a new {@link AppInstallationToken} is generated, wait this many seconds before continuing.
@@ -213,8 +215,8 @@ public class GitHubAppCredentials extends BaseStandardCredentials implements Sta
                 appInstallation = appInstallations.stream()
                     .filter(installation -> installation.getAccount().getLogin().equals(owner))
                     .findAny()
-                    .orElseThrow(() -> new IllegalArgumentException(String.format(ERROR_NOT_INSTALLED,
-                        appId)));
+                    .orElseThrow(() -> new IllegalArgumentException(String.format(ERROR_NO_OWNER_MATCHING,
+                        appId, owner)));
             }
 
             GHAppInstallationToken appInstallationToken = appInstallation
