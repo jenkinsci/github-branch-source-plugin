a7a0771394138328953547039a8bb682760669bf
Gavin Mogan <github@gavinmogan.com>
2021-04-28 15:14:59 -0700

    Update src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.java
    Co-authored-by: Liam Newman <bitwiseman@gmail.com>

--- src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.java
@@ -1101,8 +1101,8 @@ public class GitHubSCMNavigator extends SCMNavigator {
                           String.format(
                               "Skipping repository %s because it is public", repo.getName())));
 
-            } else if (repo.getSource() != null
-                && gitHubSCMNavigatorContext.isExcludeForkedRepositories()) {
+            } else if (gitHubSCMNavigatorContext.isExcludeForkedRepositories()
+                && repo.getSource() != null) {
               witness.record(repo.getName(), false);
               listener
                   .getLogger()
