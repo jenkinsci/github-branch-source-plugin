e3ad04366fddd9d8c94005aefe0e6f65573c2ecf
cwholmes <cholmes5324@gmail.com>
2021-08-31 19:41:02 -0500

    Fix ignore drafts trait for only github sources

--- src/main/java/org/jenkinsci/plugins/github_branch_source/IgnoreDraftPullRequestFilterTrait.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/IgnoreDraftPullRequestFilterTrait.java
@@ -61,5 +61,15 @@ public class IgnoreDraftPullRequestFilterTrait extends SCMSourceTrait {
     public String getDisplayName() {
       return Messages.IgnoreDraftPullRequestFilterTrait_DisplayName();
     }
+
+    @Override
+    public Class<? extends SCMSourceContext> getContextClass() {
+      return GitHubSCMSourceContext.class;
+    }
+
+    @Override
+    public Class<? extends SCMSource> getSourceClass() {
+      return GitHubSCMSource.class;
+    }
   }
 }
