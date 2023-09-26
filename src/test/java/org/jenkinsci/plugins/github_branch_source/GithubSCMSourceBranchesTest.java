a502178d0be118f4023f81b889fee70f87de6b2f
arus2023 <arus6053@gmail.com>
2023-09-25 22:02:59 -0500

    Removed 1 unnecessary stubbing in GithubSCMSourceBranchesTest.java

--- src/test/java/org/jenkinsci/plugins/github_branch_source/GithubSCMSourceBranchesTest.java
+++ src/test/java/org/jenkinsci/plugins/github_branch_source/GithubSCMSourceBranchesTest.java
@@ -80,7 +80,6 @@ public class GithubSCMSourceBranchesTest extends GitSCMSourceBase {
         context.wantBranches(true);
         GitHubSCMSourceRequest request =
                 context.newRequest(new GitHubSCMSource("cloudbeers", "yolo", null, false), null);
-        Mockito.doThrow(e).when(repoSpy).getRef("branches/existent-branch");
         // Expected: This will throw an error when requesting a branch
         try {
             Iterator<GHBranch> branches = new GitHubSCMSource.LazyBranches(request, repoSpy).iterator();
