4c250951d4602ef4cc33dea61377834aa61fdccb
Olivier Lamy <olamy@apache.org>
2023-11-15 09:52:11 +1000

    Do not enforce GitHub app to comes from the same org as the repo org. The GH app can come from another org as log as it is installed in the org with the target git repo there is no security issue (#744)
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubAppCredentials.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubAppCredentials.java
@@ -326,9 +326,6 @@ public class GitHubAppCredentials extends BaseStandardCredentials implements Sta
     @NonNull
     public synchronized GitHubAppCredentials withOwner(@NonNull String owner) {
         if (this.owner != null) {
-            if (!owner.equals(this.owner)) {
-                throw new IllegalArgumentException("Owner mismatch: " + this.owner + " vs. " + owner);
-            }
             return this;
         }
         if (byOwner == null) {
