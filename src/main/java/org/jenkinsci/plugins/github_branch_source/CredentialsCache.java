82a5871dc9501fe0d04a17015344bbf098d63ed9
Allan Burdajewicz <aburdajewicz@cloudbees.com>
2024-05-16 23:05:17 +1000

    [JENKINS-73172] Handle expiration

--- src/main/java/org/jenkinsci/plugins/github_branch_source/CredentialsCache.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/CredentialsCache.java
@@ -7,18 +7,26 @@ import hudson.model.Item;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
+import jenkins.util.SystemProperties;
 
 /**
  * Caches the {@link com.cloudbees.plugins.credentials.common.StandardCredentials} per credentials request based on
- * context, apiUri and credentialsId. The credentials are cached within a thread context using
- * a {@link java.lang.ThreadLocal}. Typically, this would yield a lot of cache it in repeated usage within the same
- * single thread process such as Branch Indexing and Organization Scans.
+ * context, apiUri and credentialsId. The credentials are cached within a thread context using a {@link java.lang.ThreadLocal}.
+ * High cache hit ratio is expected in repeated usage within the same single threaded process such as Branch Indexing
+ * and Organization Scans.
  */
 public class CredentialsCache {
 
-    private static final ThreadLocal<Map<CredentialsRequest, StandardCredentials>> scanCredentialsRequests =
-            ThreadLocal.withInitial(HashMap::new);
+    /**
+     * The cache expiry in minutes. Default to 10 minutes to stay within the acceptable staleness behavior of
+     * {@link GitHubAppCredentials}.
+     */
+    private static final long CACHE_DURATION_SECONDS = SystemProperties.getLong(
+            CredentialsCache.class.getName() + ".CACHE_DURATION_SECONDS", TimeUnit.MINUTES.toSeconds(1L));
+
+    private static final ThreadLocal<Map<Integer, CacheValue>> scanCredentialsRequests = new ThreadLocal<>();
 
     /**
      * Retrieve the cached credentials in the specified context for use against the specified API endpoint. Otherwise
@@ -26,58 +34,49 @@ public class CredentialsCache {
      *
      * @param context the context.
      * @param apiUri the API endpoint.
-     * @param scanCredentialsId the credentials to resolve.
+     * @param credentialsId the credentials to resolve.
      * @return the {@link StandardCredentials} or {@code null}
      */
     @CheckForNull
     static StandardCredentials getOrCreate(
             @CheckForNull Item context,
             @CheckForNull String apiUri,
-            @CheckForNull String scanCredentialsId,
+            @CheckForNull String credentialsId,
             Supplier<StandardCredentials> lookup) {
-        if (Util.fixEmpty(scanCredentialsId) == null) {
+        if (Util.fixEmpty(credentialsId) == null) {
             return null;
         }
-        CredentialsRequest credentialsRequest =
-                CredentialsRequest.from(apiUri, scanCredentialsId, context == null ? null : context.getFullName());
-        StandardCredentials result = scanCredentialsRequests.get().get(credentialsRequest);
-        if (result == null) {
-            scanCredentialsRequests.get().put(credentialsRequest, lookup.get());
+
+        if (scanCredentialsRequests.get() == null) {
+            scanCredentialsRequests.set(new HashMap<>());
         }
-        return scanCredentialsRequests.get().get(credentialsRequest);
+
+        int cacheKey = Objects.hash(apiUri, credentialsId, context == null ? null : context.getFullName());
+        CacheValue result = scanCredentialsRequests.get().get(cacheKey);
+        if (result == null || result.expired()) {
+            result = new CacheValue(lookup.get());
+            scanCredentialsRequests.get().put(cacheKey, result);
+        }
+        return result.getCredentials();
     }
 
-    private static class CredentialsRequest {
+    private static class CacheValue {
 
-        private final String apiUri;
+        private final StandardCredentials credentials;
 
-        private final String credentialsId;
+        private final long requestTimeInMs;
 
-        private final String context;
-
-        CredentialsRequest(String apiUri, String credentialsId, String context) {
-            this.apiUri = apiUri;
-            this.credentialsId = credentialsId;
-            this.context = context;
+        public CacheValue(StandardCredentials credentials) {
+            this.credentials = credentials;
+            this.requestTimeInMs = System.currentTimeMillis();
         }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof CredentialsRequest)) return false;
-            CredentialsRequest that = (CredentialsRequest) o;
-            return Objects.equals(apiUri, that.apiUri)
-                    && Objects.equals(credentialsId, that.credentialsId)
-                    && Objects.equals(context, that.context);
+        public StandardCredentials getCredentials() {
+            return credentials;
         }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(apiUri, credentialsId, context);
-        }
-
-        static CredentialsRequest from(String apiUri, String credentialsId, String context) {
-            return new CredentialsRequest(apiUri, credentialsId, context);
+        public boolean expired() {
+            return (System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(CACHE_DURATION_SECONDS)) > requestTimeInMs;
         }
     }
 }
