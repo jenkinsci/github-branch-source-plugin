35aafa207e2f1f4575556004d36c33a48818c3b6
Jérôme Pochat <jpochat@cloudbees.com>
2024-10-01 16:55:46 +0200

    [JENKINS-73474] Cleanup

--- src/test/java/org/jenkinsci/plugins/github_branch_source/GithubAppCredentialsContextualizationTest.java
+++ src/test/java/org/jenkinsci/plugins/github_branch_source/GithubAppCredentialsContextualizationTest.java
@@ -10,6 +10,7 @@ import static org.hamcrest.Matchers.notNullValue;
 
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.domains.Domain;
+import hudson.model.Action;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
@@ -18,7 +19,6 @@ import java.util.Arrays;
 import java.util.Date;
 import jenkins.branch.BranchSource;
 import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
-import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProjectTest;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -44,17 +44,8 @@ public class GithubAppCredentialsContextualizationTest extends AbstractGitHubWir
                         false))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json; charset=utf-8")
-                        .withBody("{\n"
-                                + "  \"token\": \"super-secret-token\",\n"
-                                +
-                                // This token will go stale at the soonest allowed time but will not
-                                // expire for the duration of the test
-                                "  \"expires_at\": \""
-                                + printDate(new Date(System.currentTimeMillis()
-                                        + Duration.ofMinutes(10).toMillis()))
-                                + "\""
-                                + // 2019-08-10T05:54:58Z
-                                "}")));
+                        .withBody("{\"token\":\"super-secret-token\",\"expires_at\":\"" + createTokenExpiration()
+                                + "\"}")));
 
         // Add wiremock responses for Repository
         githubApi.stubFor(get(urlEqualTo("/repos/cloudbeers/multibranch-demo"))
@@ -90,14 +81,19 @@ public class GithubAppCredentialsContextualizationTest extends AbstractGitHubWir
 
         final var multiBranchProject = r.jenkins.createProject(WorkflowMultiBranchProject.class, "multibranch-demo");
         multiBranchProject.getSourcesList().add(new BranchSource(scmSource));
+        multiBranchProject.scheduleBuild2(0, new Action[0]).getFuture().get();
 
-        final var branchProject =
-                WorkflowMultiBranchProjectTest.scheduleAndFindBranchProject(multiBranchProject, "master");
+        final var branchProject = multiBranchProject.getItem("master");
         assertThat(branchProject, notNullValue());
     }
 
-    static String printDate(Date dt) {
-        return DateTimeFormatter.ISO_INSTANT.format(
-                Instant.ofEpochMilli(dt.getTime()).truncatedTo(ChronoUnit.SECONDS));
+    private String createTokenExpiration() {
+        // This token will go stale at the soonest allowed time but will not
+        // expire for the duration of the test
+        // Format: 2019-08-10T05:54:58Z
+        return DateTimeFormatter.ISO_INSTANT.format(Instant.ofEpochMilli(new Date(System.currentTimeMillis()
+                                + Duration.ofMinutes(10).toMillis())
+                        .getTime())
+                .truncatedTo(ChronoUnit.SECONDS));
     }
 }
