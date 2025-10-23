0f92de88a68c84d8f0748cf6bf8f9dd2d2d91346
Albert Puig <apuig@cloudbees.com>
2025-10-23 18:14:05 +0200

    [JENKINS-76235] Fix 500 error when saving organization folder with credentials using an invalid owner

--- src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.java
+++ src/main/java/org/jenkinsci/plugins/github_branch_source/GitHubSCMNavigator.java
@@ -1696,7 +1696,7 @@ public class GitHubSCMNavigator extends SCMNavigator {
             } finally {
                 Connector.release(hub);
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             DescriptorImpl.LOGGER.log(Level.WARNING, e.getMessage(), e);
         }
     }
